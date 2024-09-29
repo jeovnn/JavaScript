50673cc6632ebe9e7a3a91b9ede9fa67e57a590c
Jeovan <jeovandeabreu09@gmail.com>
2024-09-29 11:00:08 -0300

    Adicionando arquivos iniciais

--- 1001.js
+++
@@ -1,8 +0,0 @@
-let input = require('fs').readFileSync('./dev/stdin/file.txt', 'utf8');
-let linhas = input.split('/n');
-
-let x = +linhas.shift();
-let y = +linhas.shift();
-
-console.log(`X = ${x + y}`);
-
